d0c4eb10d9c37278e064cc2fe6cda38d5c58167c
Frank Schmidt <frank.schmidt@redspace.com>
2017-05-04 10:55:09 -0300

    fetch the score from our service

--- src/app/results/results/results.component.ts
+++ src/app/results/results/results.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FlashCardService } from '../../survey/flash-card.service';
 
 @Component({
   selector: 'app-results',
@@ -6,10 +7,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./results.component.scss']
 })
 export class ResultsComponent implements OnInit {
+  score:number
+  name: string
 
-  constructor() { }
+  constructor( private service:FlashCardService ) { }
 
   ngOnInit() {
+    this.getData()
+  }
+
+  getData(): void {
+    this.score = this.service.getScore()
+    this.name = this.service.getName()
   }
 
 }
